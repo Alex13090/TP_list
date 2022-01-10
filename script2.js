0f73c43fa10f7b49315e50a27290b64bee754b60
Alex13090 <kohninnaung@gmail.com>
2022-01-10 14:25:09 +0100

    2nd partie

--- script2.js
+++ script2.js
@@ -2,7 +2,7 @@
 //Variable qui récupére le modal
 let modal = document.getElementById("myModal");
 //Récupération du <span> pour fermer le modal
-/*
+
 let span = document.getElementsByClassName("close")[0];
 //fonction fermeture du modal quand l'utilisateur clic sur le <span> (x)
 span.onclick = function() {
@@ -16,29 +16,43 @@ function openModal(e){
     //injection du bouton dans le paragraphe
     btUpdate.innerHTML = "<input type='button' value='Modifier' onclick='updateUser("+e+")'></input>";
 }
-*/
+
+// get ELMT
 let nbrCpt = document.getElementById("nbrCompte");
+// create table
 let list = [];
+// create nbr list
 let listNbr = 0;
 let addToList = document.getElementById("resultat");
 let error = document.getElementById("error");
-
+// create img & link for delete
+let btnDelete = document.createElement("a");
+btnDelete.href = "#";
+btnDelete.id = "btnDelete";
+btnDelete.onclick = "deleteBtn()";
+btnDelete.innerHTML = "<img src='delete.png' alt='delete'>";
+// function onclick
 function ajouter() {
+    // get element
     let name = document.getElementById("name").value;
     let fstName = document.getElementById("firstName").value;
     let login = document.getElementById("login").value;
     let passWord = document.getElementById("mdp").value;
+    // if 
     if(name != "" && fstName != "" && login != "" && passWord != "") {
+        // push to tab
         list.push([name, fstName, login, passWord]);
-        addToList.innerHTML += "<p>"+`name: ${name}; prenom: ${fstName}; login: ${login}; mot de passe: ${passWord}`+"</p>";
+        // innerHTML with "p" separator
+        addToList.innerHTML += "<p>"+`${listNbr+1}.  name : ${name};  prénom : ${fstName};  login : ${login};  mot de passe : ${passWord} ${btnDelete} ${btUpdate}`+"</p>";
+        // up nbr list
         listNbr++;
-
         if (listNbr == 1) {
             nbrCpt.innerHTML = `Il y a ${listNbr} compte utilisateur`;
         } else if (listNbr > 1) {
             nbrCpt.innerHTML = `Il y a ${listNbr} compte utilisateurs`;
         }
+    } else {
+        error.innerHTML = `Veuillez remplir les champs vites:`;
     }
-    
 }
  
